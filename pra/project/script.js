f30baeae925038ddf96d3e452a4e1dd03c3cbb56
Raga-M <ragavendran200@gmail.com>
2023-03-09 19:37:44 +0530

    js

--- pra/project/script.js
+++ pra/project/script.js
@@ -1,6 +1,15 @@
 const icon = document.querySelector('.icon');
 const nav = document.querySelector('nav ul');
-
 icon.addEventListener('click', () => {
  nav.classList.toggle('show');
+ icon.style.transition = '0.5s';
+ if(icon.innerText==="✕"){
+ 
+    icon.innerHTML='&#9776';}
+    else{
+        // icon.style.transform= '0.8s';
+        icon.innerHTML='&#10005';}
+
+
+
 });
