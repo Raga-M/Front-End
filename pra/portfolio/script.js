caaaf94bc8d6b285253b5fdcf0f18feb47163864
Raga-M <ragavendran200@gmail.com>
2023-03-18 18:28:29 +0530

    portfolio site

---
+++ pra/portfolio/script.js
@@ -0,0 +1,20 @@
+const hamburger=document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile=document.querySelector('.header .nav-bar .nav-list ul');
+const menu=document.querySelectorAll('.header .nav-bar .nav-list ul a');
+const header=document.querySelector('.header.container')
+hamburger.addEventListener('click',()=>{
+hamburger.classList.toggle('active')
+mobile.classList.toggle('active')})
+document.addEventListener('scroll',()=>{
+var scroll_position=window.scrollY;
+if(scroll_position > 250)
+{
+    header.style.backgroundColor="#29323c";
+}
+else{
+    header.style.backgroundColor="transparent";
+}})
+menu.forEach(item=>{
+item.addEventListener('click',()=>{
+    hamburger.classList.toggle('active')
+    mobile.classList.toggle('active')})})
