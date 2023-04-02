19c099c2b30944fb3b6991264ba7ab2fd4ec183b
Raga-M <ragavendran200@gmail.com>
2023-04-02 17:33:30 +0530

    project

--- pra/project/script.js
+++ pra/project/script.js
@@ -1,15 +1,5 @@
-const icon = document.querySelector('.icon');
-const nav = document.querySelector('nav ul');
+const icon = document.querySelector('.navbar ');
+
 icon.addEventListener('click', () => {
- nav.classList.toggle('show');
- icon.style.transition = '0.5s';
- if(icon.innerText==="✕"){
- 
-    icon.innerHTML='&#9776';}
-    else{
-        // icon.style.transform= '0.8s';
-        icon.innerHTML='&#10005';}
-
-
-
+ icon.classList.toggle('show');
 });
